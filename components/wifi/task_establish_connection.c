6e2a918e397a555d56893ee854701738018f853d
Ada Piekarska <adapiekarska97@gmail.com>
2019-08-15 14:46:23 +0200

    Initial working setup

---
+++ components/wifi/task_establish_connection.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+#include "include/task_establish_connection.h"
+
+void test() {
+    printf("test\n");
+}
---
+++ main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#include "task_establish_connection.h"
+
+void app_main()
+{
+    test();
+}
