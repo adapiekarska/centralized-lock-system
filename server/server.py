769cd909ede00dfe340250f9d4ef1013a06200ca
Ada Piekarska <adapiekarska97@gmail.com>
2019-10-19 16:54:01 +0200

    Modify server script to support SSL

--- server/server.py
+++ server/server.py
@@ -1,54 +1,62 @@
-# This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-# Unless required by applicable law or agreed to in writing, this
-# software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied.
-
-# -*- coding: utf-8 -*-
-
 import socket
 import sys
+import ssl
 
 # -----------  Config  ----------
-IP_VERSION = 'IPv4'
 PORT = 3333
 # -------------------------------
 
-if IP_VERSION == 'IPv4':
+server_cer  = 'server.cer'
+server_key  = 'server.key'
+ca_cer      = 'ca.cer'
+
+def create_socket():
     family_addr = socket.AF_INET
-elif IP_VERSION == 'IPv6':
-    family_addr = socket.AF_INET6
-else:
-    print('IP_VERSION must be IPv4 or IPv6')
-    sys.exit(1)
+    try:
+        sock = socket.socket(family_addr, socket.SOCK_STREAM)
+    except socket.error as msg:
+        print('Error: ' + str(msg[0]) + ': ' + msg[1])
+        sys.exit(1)
+    return sock
 
+def create_ssl_context():
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_cert_chain(certfile=server_cer, keyfile=server_key)
+    context.load_verify_locations(cafile=ca_cer)
+    return context
 
-try:
-    sock = socket.socket(family_addr, socket.SOCK_STREAM)
-except socket.error as msg:
-    print('Error: ' + str(msg[0]) + ': ' + msg[1])
-    sys.exit(1)
+def handle_client(connstream):
+    data = connstream.recv(1024)
+    while data:
+        data = data.decode()
+        print('Received data: ' + data)
+        reply = 'OK: ' + data
+        conn.send(reply.encode())
+        data = connstream.recv(1024)
 
-print('Socket created')
+def main():
+    sock = create_socket()
+    ssl_context = create_ssl_context()
+    try:
+        sock.bind(('', PORT))
+        print('Socket binded')
+        sock.listen(1)
+        print('Socket listening')
 
-try:
-    sock.bind(('', PORT))
-    print('Socket binded')
-    sock.listen(1)
-    print('Socket listening')
-    conn, addr = sock.accept()
-    print('Connected by', addr)
-except socket.error as msg:
-    print('Error: ' + str(msg[0]) + ': ' + msg[1])
-    sock.close()
-    sys.exit(1)
+        while True:
+            newsocket, fromaddr = sock.accept()
+            print('Connected by', fromaddr)
+            connstream = ssl_context.wrap_socket(newsocket, server_side= True)
+            try:
+                handle_client(connstream)
+            finally:
+                connstream.shutdown(socket.SHUT_RDWR)
+                connstream.close()
+    except socket.error as msg:
+        print('Error: ' + str(msg[0]) + ': ' + msg[1])
+        sock.close()
+        sys.exit(1)
 
-while True:
-    data = conn.recv(128)
-    if not data:
-        break
-    data = data.decode()
-    print('Received data: ' + data)
-    reply = 'OK: ' + data
-    conn.send(reply.encode())
-conn.close()
+if __name__ == "__main__":
+    main()
