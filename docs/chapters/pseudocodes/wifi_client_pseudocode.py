d1d76d1a96147715759d2cefd4dfaef2dfcd1bbb
Grzegorz Was <grzegorz.was@intel.com>
2019-11-11 15:37:14 +0100

    update wifi-client pseudocode

--- docs/chapters/pseudocodes/wifi_client_pseudocode.py
+++ docs/chapters/pseudocodes/wifi_client_pseudocode.py
@@ -1,37 +1,25 @@
 def wifi_client_start():
     # WiFi Client should wait for connection to AP
-    wait_event(WIFI_CONNECTED, DONT_CLEAR)
-
-    Status status
+    wait_event(WIFI_CONNECTED)
 
     while (TRUE):
         status = wifi_socket_connect()
         if (status != SUCCESS)
             break
-
         # Communication loop
         while (TRUE):
-            LOG("wifi_client", "Ready to transmit")
-            notify_event(WIFI_CLIENT_READY)
-
             # Wait for pending operation
-            wait_event(
-                WIFI_CLIENT_SEND_PENDING or WIFI_CLIENT_RECEIVE_PENDING,
-                DONT_CLEAR
-                )
+            wait_event(WIFI_CLIENT_SEND_PENDING or WIFI_CLIENT_RECEIVE_PENDING)
             if (event_pending(WIFI_CLIENT_SEND_PENDING)):
                 # Requested operation is SEND
-                clear_event(WIFI_CLIENT_SEND_PENDING)
-                status = wifi_socket_transfer_data(pending_data, pending_data_size);
+                status = wifi_socket_send(data_to_send)
                 if (status != SUCCESS):
                     # Notify transmission fail
                     notify_event(WIFI_CLIENT_TRANSMISSION_FAIL)
                     break
-
             else:
                 # Requested operation is RECEIVE
-                clear_event(WIFI_CLIENT_RECEIVE_PENDING)
-                status = wifi_socket_receive_data(pending_data, pending_data_size)
+                status, received_data = wifi_socket_receive_data()
                 if (status != SUCCESS):
                     # Notify transmission fail
                     notify_event(WIFI_CLIENT_TRANSMISSION_FAIL)
